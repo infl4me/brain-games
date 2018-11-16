96de290dd00a1d80172025067bcec053e59abd6b
infl4me <hellpl4y@gmail.com>
2018-11-17 02:58:22 +0500

    refactoring game menu

--- src/index.js
+++ src/index.js
@@ -9,27 +9,27 @@ import runBrainPrime from './games/prime';
 const gameList = ['1. brain-even', '2. brain-calc', '3. brain-gcd', '4. brain-progression', '5. brain-prime'];
 const games = [null, runBrainEven, runBrainCalc, runBrainGcd, runBrainProgression, runBrainPrime];
 
-const runGameSelection = (userNameFromMenu) => {
+const runGameSelection = (userName) => {
   console.log('\nHere is a list of the games:\n');
   console.log(gameList.join('\n'));
 
   const userChosenGame = readlineSync.question('\nType a number of the game you want to play or "exit" to quit: \n');
   if (games[userChosenGame]) {
-    games[userChosenGame](userNameFromMenu);
+    games[userChosenGame](userName);
   } else if (userChosenGame === 'exit') {
     return;
   } else {
     console.log('Number not found.\n');
   }
 
-  runGameSelection(userNameFromMenu);
+  runGameSelection(userName);
 };
 
 export default () => {
   console.log(`${welcomeMessage}\n`);
 
-  const userNameFromMenu = getName();
-  console.log(`Hello, ${userNameFromMenu}!`);
+  const userName = getName();
+  console.log(`Hello, ${userName}!`);
 
-  runGameSelection(userNameFromMenu);
+  runGameSelection(userName);
 };
