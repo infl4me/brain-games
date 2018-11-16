fcb47574a1e4aafd7d6496f9a5d860d2d15ebe77
infl4me <hellpl4y@gmail.com>
2018-11-17 01:15:37 +0500

    fix game menu

--- src/index.js
+++ src/index.js
@@ -7,33 +7,21 @@ import runBrainProgression from './games/progression';
 import runBrainPrime from './games/prime';
 
 const gameList = ['1. brain-even', '2. brain-calc', '3. brain-gcd', '4. brain-progression', '5. brain-prime'];
+const games = [null, runBrainEven, runBrainCalc, runBrainGcd, runBrainProgression, runBrainPrime];
 
 const runGameSelection = (userNameFromMenu) => {
   console.log('\nHere is a list of the games:\n');
   console.log(gameList.join('\n'));
-  const userChosenGame = readlineSync.question('\nType a number of the game you want to play or "exit" to quit: \n');
 
-  switch (userChosenGame) {
-    case '1':
-      runBrainEven(userNameFromMenu);
-      break;
-    case '2':
-      runBrainCalc(userNameFromMenu);
-      break;
-    case '3':
-      runBrainGcd(userNameFromMenu);
-      break;
-    case '4':
-      runBrainProgression(userNameFromMenu);
-      break;
-    case '5':
-      runBrainPrime(userNameFromMenu);
-      break;
-    case 'exit':
-      return;
-    default:
-      console.log('Number not found.\n');
+  const userChosenGame = readlineSync.question('\nType a number of the game you want to play or "exit" to quit: \n');
+  if (games[userChosenGame]) {
+    games[userChosenGame](userNameFromMenu);
+  } else if (userChosenGame === 'exit') {
+    return;
+  } else {
+    console.log('Number not found.\n');
   }
+
   runGameSelection(userNameFromMenu);
 };
 
